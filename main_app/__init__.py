4484383c56da3b7636c859715c5e3638ff40c0ac
a1png <a1png.chan@gmail.com>
2018-05-03 21:09:18 +1000

    flask initiated

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -9,14 +9,13 @@ db = SQLAlchemy()
 
 def create_app(config_name=None):
     app = Flask(__name__)
-    
+    register_blueprints(app)
     return app
-
-
 
 
 def register_blueprints(app):
     from main_app.hello import bp_hello
+    print(bp_hello.bp)
     app.register_blueprint(bp_hello.bp, url_prefix='/hello')
 
 
--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,6 @@
 from main_app import create_app
 app = create_app()
 if __name__ == '__main__':
+    print(app)
     app.run()
 
